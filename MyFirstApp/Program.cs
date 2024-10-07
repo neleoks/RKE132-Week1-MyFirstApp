d4623be2f7ed6bf49a6250f7a8cda6f1da4b3b4c
Nele Oks <nele.oks@tktk.ee>
2024-10-07 20:14:59 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+
+// Rakendus küsib kasutajal sisestada tema nime
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+
+//string-sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
